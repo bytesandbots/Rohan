1cea16b1f97f19242556766818a30f21d176b620
bytesandbots <124220094+bytesandbots@users.noreply.github.com>
2025-03-24 19:46:29 -0700

    Create bytes and bots LMS
    Making a system that can log info about students and can be managed by mentors

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ import flask
 import psycopg2
 import bcrypt 
 import datetime
-
+from datetime import date
+import uuid
+import json
 
 #addinfo
 def addinfo(info):
@@ -15,6 +17,10 @@ def addinfo(info):
     age=info.get("age")
     dob=info.get("dob")
     role=info.get("role")
+    homeaddress=info.get("adress")
+    email=info.get("email")
+    phonenumber=info.get("phonenumber")
+    
     
     username=flask.session.get("username")
     conn=psycopg2.connect(URL)
@@ -23,12 +29,12 @@ def addinfo(info):
     info= cursor.fetchall()
     if info == []:
 
-        cursor.execute("INSERT INTO person_information (username, Firstname, Lastname, city, statee, country, age, dob, role)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);",
-                       (username, fname, lname, city, state, country, age, dob, role))
+        cursor.execute("INSERT INTO person_information (username, Firstname, Lastname, city, statee, country, age, dob, role, adress, email, phonenumber)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+                       (username, fname, lname, city, state, country, age, dob, role, homeaddress, email, phonenumber))
         conn.commit()
     else:
-        cursor.execute("update person_information set Firstname=%s, Lastname=%s, city=%s, statee=%s, country=%s, age=%s, dob=%s, role=%s where username=%s",
-                       ( fname, lname, city, state, country, age, dob, role, username))
+        cursor.execute("update person_information set Firstname=%s, Lastname=%s, city=%s, statee=%s, country=%s, age=%s, dob=%s, role=%s, adress=%s, email=%s, phonenumber=%s where username=%s",
+                       ( fname, lname, city, state, country, age, dob, role, homeaddress, email, phonenumber, username))
         conn.commit()
     conn.close()
     return flask.redirect("/")
@@ -58,32 +64,116 @@ def deleteuser():
     
 
 
-#def clearhrs
-@app.route("/clearhrs")
-def clearhrs():
-    username=flask.session.get("username")
-    conn=psycopg2.connect(URL)
-    cursor=conn.cursor()    
-    cursor.execute("DELETE FROM timeworked WHERE username = %s ",
-                       (username, ))
+#mentor check
+def mentorcheck():
+    if flask.session.get("is_logged_in"):
+        username=flask.session.get("username")
+        conn=psycopg2.connect(URL)
+        cursor=conn.cursor()
+        cursor.execute("select role from person_information where username=%s;", (username,))
+        info= cursor.fetchall()
+        mentor=False
+        if (info!=None):
+            if (info[0][0] == "mentor"):
+                mentor=True
+        return mentor
 
-    conn.commit()
-    conn.close()
+#render mentor page
+@app.route("/student")
+def student():
+     if flask.session.get("is_logged_in"):
+        username=flask.session.get("username")
+        conn=psycopg2.connect(URL)
+        cursor=conn.cursor()
+        
+        cursor.execute("select role from person_information where username=%s;", (username,))
+
+        info= cursor.fetchall()
+
+        mentor=False
+        if (info!=None):
+            if (info[0][0] == "mentor"):
+                mentor=True
+        info= cursor.fetchall()
+        
+     
+          
+
+        return flask.render_template("/student.html", admin=admincheck, mentor=mentorcheck())
+#searching students
+@app.route("/searchstudents", methods=["POST"])
+def searchstudent():
+    info=flask.request.form.get("studentsearch")
+    conn=psycopg2.connect(URL)
+    cursor=conn.cursor()
+    
+    info = "%"+info+"%"
+    cursor.execute("select fname, lname, studentid from studentinfo where fname LIKE %s;", (info,))
+    info= cursor.fetchall()
+
+
+
 
     
-    return flask.redirect("/yourinfo")
-#def clear everyones hrs
-@app.route("/deleteallhrs")
-def deleteallhrs():
-    conn=psycopg2.connect(URL)
-    cursor=conn.cursor()    
-    cursor.execute("TRUNCATE TABLE timeworked;")
+    return flask.render_template("/student.html", admin=admincheck(), mentor=mentorcheck(), searchstudent=info)
+
+#creating a page for each individual student
+@app.route("/studentpage/<studentid>")
+def studentpage(studentid):
     
-    conn.commit()
-    conn.close()
-    return flask.redirect("/admin")
- 
-    
+    if flask.session.get("is_logged_in"):
+        username=flask.session.get("username")
+        flask.session["takeid"]= studentid
+        conn=psycopg2.connect(URL)
+        cursor=conn.cursor()
+        cursor.execute("select * from studentinfo where studentid=%s;", (studentid,))
+        info= cursor.fetchone()
+        cursor.execute("select * from studentdebrief where studentid=%s;", (studentid,))
+        debriefs = cursor.fetchall()
+        if debriefs == []:
+            debriefs = False
+        cursor.execute("select * from studentreport where studentid=%s;", (studentid,))
+        reports = cursor.fetchall()
+        cursor.execute("select * from checkin where studentid=%s;", (studentid,))
+        checkedin = cursor.fetchall()
+        if checkedin ==[]:
+            checksin = False
+        cursor.execute("select * from youtubevideos where studentid=%s order by videoid desc;", (studentid,))
+        videos = cursor.fetchall()
+        videosuploaded = True
+        if videos == []:
+            videosuploaded = False
+        conn.close()
+        if info ==[]:
+            return flask.render_template("/studentpage.html", found = False, admin = admincheck(), info = [], mentor=mentorcheck(), debriefs = [], reports = [] ) 
+        return flask.render_template("/studentpage.html", videos=videos, videosuploaded = videosuploaded, found =True, admin = admincheck(), info = info, mentor=mentorcheck(), debriefs = debriefs, reports = reports )            
+
+@app.route("/checkin/<studentid>")
+def checkin(studentid):
+    if flask.session.get("is_logged_in"):
+        username=flask.session.get("username")
+        conn=psycopg2.connect(URL)
+        cursor=conn.cursor()
+        studentid = flask.session.get("takeid")
+        checkintime = datetime.datetime.now()
+        cursor.execute("INSERT INTO checkin (studentid, checkintime)VALUES (%s, %s);",(studentid, checkintime))
+        conn.commit()
+        conn.close()
+        return flask.redirect("/studentpage/" + studentid)
+
+@app.route("/checkout/<studentid>")
+def checkout(studentid):
+    if flask.session.get("is_logged_in"):
+        username=flask.session.get("username")
+        conn=psycopg2.connect(URL)
+        cursor=conn.cursor()
+        studentid = flask.session.get("takeid")
+        cursor.execute("DELETE FROM checkin WHERE studentid = %s ",(studentid, ))
+        conn.commit()
+        conn.close()
+        return flask.redirect("/studentpage/" + studentid)
+
+        
 #admin check
 def admincheck():
     if flask.session.get("is_logged_in"):
@@ -108,30 +198,29 @@ def admin():
         cursor=conn.cursor()
         cursor.execute("select role from login where username=%s;", (username,))
         info= cursor.fetchall()
-        print (info)
         admin=False
         if (info!=None):
             if (info[0][0] == "Admin"):
                 admin=True
-        cursor.execute("select username, total from timeworked ", ())
         info= cursor.fetchall()
-        alltotals = {}
-        everyonetotal = 0
-        for username in info:
-            user, total = username
-            everyonetotal += total
-            if user in alltotals:
-                alltotals[user]+= int(total)
-                
-            else:
-                alltotals[user]=int(total)
+        
           
 
-        return flask.render_template("/admin.html", admin=admincheck(), final_total=alltotals, everyonetotals=everyonetotal)
+        return flask.render_template("/admin.html", admin=admincheck(), mentor=mentorcheck())
  
 
-
-
+@app.route("/creatementor")
+def creatementor():
+    if flask.session.get("is_logged_in"):
+        username=flask.session.get("username")
+        conn=psycopg2.connect(URL)
+        cursor=conn.cursor()
+        cursor.execute("select username from login where username=%s;", (username,))
+        info = cursor.fetchall()
+        cursor.execute("UPDATE person_information set role = 'Admin' where username=%s;",(project, reportdate, classcategory, studentid))
+        conn.commit()
+        conn.close()
+        return flask.redirect("/admin")
 
 
         
@@ -153,7 +242,7 @@ def signup():
 def infoform():
     if flask.session.get("is_logged_in"):
         admincheck
-        return flask.render_template("info_form.html", admin=admincheck())
+        return flask.render_template("info_form.html", admin=admincheck(), mentor=mentorcheck())
     else:
         return flask.render_template("login.html")
     
@@ -165,16 +254,15 @@ def yourinfo():
         cursor=conn.cursor()
         cursor.execute("select * from person_information where username=%s;", (username,))
         info= cursor.fetchall()
-        total=totaltotalhrs()
         if info ==[]:
-            return flask.render_template("your_info.html", datainfo=info, filled=False, totaltotal=total)
+            return flask.render_template("your_info.html", datainfo=info, filled=False)
    
         info=info[0]
         for collum in info:
             if collum == "":
                 return flask.render_template("your_info.html", datainfo=info, filled=False, totaltotal=total)
             
-        return flask.render_template("your_info.html", datainfo=info, filled=True, totaltotal=total, admin=admincheck())
+        return flask.render_template("your_info.html", datainfo=info, filled=True, mentor=mentorcheck(), admin=admincheck())
     else:
         return flask.render_template("login.html")    
     
@@ -186,140 +274,132 @@ def homepage():
         cursor=conn.cursor()
         cursor.execute("select * from person_information where username=%s;", (username,))
         info= cursor.fetchone()
+
+        flask.session["userinfo"]= json.dumps(info)
         filled=True
         
         if info ==[]:
             filled=False
-        cursor.execute("select timeout from timeworked where username=%s order by timein DESC;", (username,))
-        times=cursor.fetchone()
 
-        checkedin=False
-        if times==None:
-            checkedin=False
-
-
-        else :
-            times = times[0]
-            if times==None:
-                checkedin=True
-        print(times)
-        print(checkedin)
 
         
         
    
-        return flask.render_template("homepage.html", datainfo=info, filled=filled, start=checkedin, admin=admincheck())
+        return flask.render_template("homepage.html", datainfo=info, filled=filled,mentor=mentorcheck(), admin=admincheck())
 
     else:
         return flask.render_template("login.html")
-
-
-
-#time worked
-
-def timein():
-    from datetime import datetime
-    timeins = datetime.now()
-    return timeins
-
-
-
+#addstudent
+@app.route("/enrollment")
+def enrollment():
+    userinfo=json.loads(flask.session["userinfo"])
+    return flask.render_template("enrollment.html", datainfo=userinfo, admin=admincheck(), mentor=mentorcheck())
     
-def timeout():
-    from datetime import datetime
-    timeouts = datetime.now()
-    return timeouts
-
-def total_hrs():
-    total = timeout - timein
-    return total
-
-
-
-
-
-#recording timein
-@app.route("/addtime", methods=["POST"])
-def addtime():
-    username=flask.session.get("username")
+@app.route("/submitenrollment", methods=["POST"])
+def submitenrollment():
+    info=flask.request.form
+    userinfo=json.loads(flask.session["userinfo"])
+    fname=info.get("fname")
+    lname=info.get("lname")
+    dob=info.get("dob")
+    grade=info.get("grade")
+    school=info.get("school")
+    studentid=int(uuid.uuid4())
+    
     conn=psycopg2.connect(URL)
     cursor=conn.cursor()
-
-    t=timein()
-    date=datetime.datetime.now().date()
-    cursor.execute("INSERT INTO timeworked (username, timein, timeout, total, dates)VALUES (%s, %s, %s, %s, %s);",
-                           (username, t, None, None, date))
+    cursor.execute("INSERT INTO studentinfo (fname, lname, dob, grade, school, studentid)VALUES (%s, %s, %s, %s, %s, %s);",
+                       (fname, lname, dob, grade, school, studentid))
     conn.commit()
-    return flask.redirect("/")
-
-#recording time out
-@app.route("/addtimeout", methods=["POST"])
-def addtimeout():
-    username=flask.session.get("username")
-    conn=psycopg2.connect(URL)
-    cursor=conn.cursor()
-
-    cursor.execute("select timein from timeworked where username=%s order by timein DESC;", (username,))
-    timein=cursor.fetchone()
-    if timein==None:
-            checkedin=True
-
-        
-    if (timein == None):
-        return flask.redirect("/")
-    else :
-        timein = timein[0]
-        
-
+    conn.close()
+    return flask.render_template("homepage.html", datainfo=userinfo, admin=admincheck(), mentor=mentorcheck())    
     
-    to=timeout()
-
-    total=to-timein
-    date=datetime.datetime.now().date()
-    cursor.execute("UPDATE timeworked SET timeout = %s, total= %s WHERE username = %s and timein = %s;",
-                           (to,total.seconds/60, username, timein))
-    conn.commit()
-    return flask.redirect("/")
-
-
-#recording total hrs
-@app.route("/totalhrs", methods=["POST"])
-def totalhrs():
-    username=flask.session.get("username")
-    conn=psycopg2.connect(URL)
-    cursor=conn.cursor()
-
-    cursor.execute("select total from timeworked where username=%s order by timein DESC;", (username,))
-    timein=cursor.fetchone()[0]
-
-    
-    total=timeout-timein()
-    date=datetime.datetime.now().date()
-    cursor.execute("UPDATE timeworked SET total = %s WHERE username = %s and timein = %s and timeout = %s;",
-                           (total))
-
-#adding total hrs in total
-def totaltotalhrs():
-    username=flask.session.get("username")
-    conn=psycopg2.connect(URL)
-    cursor=conn.cursor()
-    cursor.execute("select total from timeworked where username=%s", (username, ))
-    total=cursor.fetchall()
-    totaltotal=0
-    for i in total:
-        if i[0]==None:
-            continue
-        else:
-            totaltotal+=i[0]
-    print (totaltotal/60)
-    return totaltotal/60
 
 
 @app.route("/infolocation", methods=["POST"])
 def infolocation():
+    
     info=flask.request.form
-    print("here")
-    return addinfo(info)
+    conn=psycopg2.connect(URL)
+    cursor=conn.cursor()
+    cursor.execute("insert * from login where username=%s;", (username,))
+    info= cursor.fetchall()
+#deleting debriefs
+@app.route("/deletedebrief/<debriefid>")
+def deletedebrief(debriefid):
+    conn=psycopg2.connect(URL)
+    cursor=conn.cursor()
+    studentid = flask.session.get("takeid")
+    flask.session["takeid"]= studentid
+
+    
+    cursor.execute("DELETE FROM studentdebrief where studentid = %s and debriefid=%s;", (studentid, debriefid,))
+    conn.commit()
+    conn.close()
+    return flask.redirect("/studentpage/" + studentid)
+
+#taking info from the debriefs
+@app.route("/debrieflocation", methods=["POST"])
+def debrieflocation():
+    conn=psycopg2.connect(URL)
+    cursor=conn.cursor()
+    studentid = flask.session.get("takeid")
+    info=flask.request.form
+    reportdate = date.today()
+    project = info.get("project")
+    flask.session["takeid"]= studentid
+    cursor.execute("INSERT INTO studentdebrief (mentorname, reportdate, report, studentid, project)VALUES (%s, %s, %s, %s, %s);", (info.get("mentorname"), reportdate, info.get("report"), studentid, project))
+    conn.commit()
+
+
+    conn.close()
+    checkout(studentid)
+    return flask.redirect("/studentpage/" + studentid)
+
+#Uploading youtube videos for each student
+@app.route("/videolocation", methods=["POST"])
+def videolocation():
+    
+    conn=psycopg2.connect(URL)
+    cursor=conn.cursor()
+    studentid = flask.session.get("takeid")
+    info=flask.request.form
+    flask.session["takeid"]= studentid
+    
+    videourl = info.get("videolink")
+    cuturl= videourl[videourl.index("v=")+2:]
+    videourl = cuturl[:cuturl.index("&")]
+
+    cursor.execute("INSERT INTO youtubevideos (studentid, videolink)VALUES (%s, %s);", (studentid, videourl))
+    conn.commit()
+    conn.close()
+    return flask.redirect("/studentpage/" + studentid)
+
+@app.route("/reportlocation", methods=["POST"])
+def reportlocation():
+    conn=psycopg2.connect(URL)
+    cursor=conn.cursor()
+    
+    studentid = flask.session.get("takeid")
+    info=flask.request.form
+    flask.session["takeid"]= studentid
+    cursor.execute("select * from studentreport where studentid = %s;", (studentid,))
+    student = cursor.fetchone()
+    
+    cursor.execute("select project, reportdate from studentdebrief order by reportdate desc;", (studentid, ))
+    project, reportdate = cursor.fetchone()#('classcategory', 'parent')
+    classcategory =info.get("classcategory")
+    if student == None:
+        cursor.execute("INSERT INTO studentreport (studentid, project, lastclass, classcategory)VALUES ( %s, %s, %s, %s);",( studentid, project, reportdate, classcategory))
+        conn.commit()
+    else:
+        cursor.execute("UPDATE studentreport set project = %s, lastclass = %s, classcategory =%s where studentid=%s;",(project, reportdate, classcategory, studentid))
+    conn.commit()
+        
+    conn.close()
+    return flask.redirect("/studentpage/" + studentid)
+
+
 
 
 #signup
